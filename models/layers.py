7df461c855a50efd109350167183179f1cd62c2a
Augustas Macijauskas <am3052@cam.ac.uk>
2023-02-22 16:43:10 +0000

    Testing version of layers.py done

--- models/layers.py
+++ models/layers.py
@@ -3,15 +3,33 @@ from torch import nn
 
 
 class VariationalLinear(nn.Module):
-    def __init__(self, in_features:int, out_features:int, bias=True):
+    def __init__(self, in_features:int, out_features:int, bias=True, nonlinearity="relu", param=None):
+        '''
+            nonlinearity:
+                the nonlinearity that will follow the linear layer. This will be used to 
+                calculate the gain required to properly initialize the weights.
+                For more information see
+                https://pytorch.org/docs/stable/nn.init.html#torch.nn.init.calculate_gain.
+                Default value is "relu".
+            param:
+                any parameters needed to be passed for the function that calculates the gain
+                (see also the "nonlinearity" parameter)
+        '''
+
+
         super().__init__()
 
         self.bias = bias
 
-        scale = torch.log(torch.exp(torch.sqrt(torch.tensor(2 / in_features))) - 1)
+        # Calculate gain
+        gain = torch.nn.init.calculate_gain(nonlinearity=nonlinearity, param=param)
+        print(gain)
+        # Transform the scale of gain
+        scale = torch.log(torch.exp(gain / torch.sqrt(torch.tensor(in_features))) - 1)
+        print(scale)
 
         # Note that initialization is so that initially the sampled weights follow
-        # N(0, 2 / in_features) distribution. This helps retain output distribution to
+        # N(0, gain ** 2 / in_features) distribution. This helps retain output distribution to
         # be closer to standard normal
         self.mu_weights = nn.Parameter(torch.empty(out_features, in_features).fill_(0))
         self.rho_weights = nn.Parameter(torch.empty(out_features, in_features).fill_(scale))
@@ -40,15 +58,28 @@ class VariationalLinear(nn.Module):
 
 
 def main():
-    layer = VariationalLinear(in_features=10000, out_features=1000, bias=True)
+    import torch.nn.functional as F
+
+    # layer = VariationalLinear(in_features=10000, out_features=1000, bias=True)
+    layer = VariationalLinear(in_features=10000, out_features=1000, bias=False, nonlinearity="relu")
 
     x = torch.randn((256, 10000))
     with torch.no_grad():
         out = layer(x)
+        out = F.relu(out)
+
+    print(out.mean(), out.std())
+
     
+    layer = VariationalLinear(in_features=10000, out_features=1000, bias=False, nonlinearity="tanh")
+
+    x = torch.randn((256, 10000))
+    with torch.no_grad():
+        out = layer(x)
+        out = torch.tanh(out)
+
     print(out.mean(), out.std())
     
-
 
 if __name__ == "__main__":
     main()
