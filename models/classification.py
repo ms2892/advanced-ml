Fixed ELBO calculation again, FCK ME

models/classification.py
@@ -25,7 +25,7 @@ class CrossEntropyELBO(nn.Module):
             reduction="none"
         )
 
-        return out.mean(dim=0).mean(dim=0)
+        return out.sum(dim=0).mean(dim=0)
 
 
 class Classification(nn.Module):

models/regression.py
@@ -19,7 +19,7 @@ class RegressionELBO(nn.Module):
     def _get_neg_log_lik(self, y_true, y_pred):
         batched_nll = (y_pred - y_true.unsqueeze(-1))**2 / 2
         
-        return batched_nll.mean(dim=0).mean(dim=0)
+        return batched_nll.sum(dim=0).mean(dim=0)
         
 
 